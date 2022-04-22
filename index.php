63d4238c4b00d2d8254ca7e59a00aa1eccf0f225
Yengui <ahmadyengui@yahoo.fr>
2022-04-22 02:53:13 +0100

    homepage styling and cards

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <title>Ma Gallerie</title>
     </head>
 
-    <body>
+    <body onload="javascript:controle()">
         <!--
         testtt
         <div>
@@ -62,7 +62,15 @@
             </div>
         </div>
         <section class="main">
-            test
+            <div class="maincontent">
+                <div class="maindiv">
+                    <span>Bienvenue dans<br />Ma Gallerie!</span>
+                    <p>La première gallerie tunisienne<br /> de design graphique</p>
+                </div>
+                <div class="maindiv">
+                    <img height="480px" width="640px" src="./images/mainpic.svg" alt="main illustration">
+                </div>
+            </div>
             <div class="custom-shape-divider-bottom-1650420115">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
@@ -70,7 +78,48 @@
             </div>
         </section>
         <section class="top-designs">
-            test2
+            <h1>Meilleurs designs du mois!</h1>
+            <div class="meilleur-container">
+                <div class="carte">
+                    <img class="carte-img" src="https://images.pexels.com/photos/889839/pexels-photo-889839.jpeg" alt="design 1">
+                    <div class="carte-partie2">
+                        <div class="img-info">
+                            <div class="img-titre">Dessin sur bois</div>
+                            <div class="img-description">Dessin moderne d'une femme sur bois utilisant les couleurs marron et bleu</div>
+                        </div>
+                        <div class="utilisateur-info">
+                            <img class="utilisateur-carte" src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg" alt="utilisateur 1">
+                            <div><a href="./profile/1">Ahmad Yengui</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carte">
+                    <img class="carte-img" src="https://images.pexels.com/photos/1690351/pexels-photo-1690351.jpeg" alt="design 2">
+                    <div class="carte-partie2">
+                        <div class="img-info">
+                            <div class="img-titre">Tableau artistique</div>
+                            <div class="img-description">Tableau artistique utilisant la peinture noire, bleu et jaune test test test test test test test test test test test test test test test test test test test test test test testtest test test test test test test test test test test test test test test test </div>
+                        </div>
+                        <div class="utilisateur-info">
+                            <img class="utilisateur-carte" src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg" alt="utilisateur 1">
+                            <div><a href="./profile/1">Ahmad Yengui</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carte">
+                    <img class="carte-img" src="https://images.pexels.com/photos/1477166/pexels-photo-1477166.jpeg" alt="design 3">
+                    <div class="carte-partie2">
+                        <div class="img-info">
+                            <div class="img-titre">Fleurs!</div>
+                            <div class="img-description">Image de fleurs</div>
+                        </div>
+                        <div class="utilisateur-info">
+                            <img class="utilisateur-carte" src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg" alt="utilisateur 1">
+                            <div><a href="./profile/1">Ahmad Yengui</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
         <section class="voir-designs">
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 function changeColor() {
   document.getElementById("hello").style.color = "red";
 }
+
+function controle() {
+  //controle de longueur de description d'image
+  let imageDescriptions = document.getElementsByClassName("img-description");
+  console.log(imageDescriptions);
+  for (let i = 0; i < imageDescriptions.length; i++) {
+    const element = imageDescriptions[i];
+    if (element.innerHTML.length > 100) {
+      element.innerHTML = element.innerHTML.slice(0, 100) + "...";
+    }
+  }
+}
